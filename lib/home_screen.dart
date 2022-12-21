c19e116922457df185b1c879090c1e0092efd814
Satish Gaurav <satishgaurav@summer.local>
2022-12-21 16:59:13 +0530

    diff

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,26 +3,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_tut/main.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    // final name = ref.watch(nameProvider);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final name = ref.watch(nameProvider);
+    final nameRead = ref.read(nameProvider);
+    /*
+    diff between read and watch:
+    read: only read once, after that even if there in change in provider,
+    it won't update the widgets. not recommended to use inside of build method
 
+    watch: it will constantly listen to the change in value.
+    */
     return Scaffold(
-      appBar: AppBar(),
-      body: Consumer(
-        builder: (context, ref, child){
-          final name = ref.watch(nameProvider);
-          return Column(
+      appBar: AppBar(
+        title: const Text("Satish"),
+      ),
+      body: Column(
             children: [
               Center(
                 child: Text(name),
               )
             ],
-          );
-      }),
+      ),
     );
   }
 }
